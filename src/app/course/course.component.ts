024e896958f7dae908f83e22bf111c3d013b1765
angel.gomes <angel.gomes@radancy.com>
2024-03-29 11:58:40 +0100

    ssr added and meta data

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,53 +1,72 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import {Course} from '../model/course';
-import {CoursesService} from '../services/courses.service';
-import {debounceTime, distinctUntilChanged, startWith, tap, delay} from 'rxjs/operators';
-import {merge, fromEvent} from 'rxjs';
-import {Lesson} from '../model/lesson';
-import {Meta, Title} from '@angular/platform-browser';
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { Lesson } from '../model/lesson';
+import { Meta, Title } from '@angular/platform-browser';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { NgIf } from '@angular/common';
 
-
 @Component({
-    selector: 'course',
-    templateUrl: './course.component.html',
-    styleUrls: ['./course.component.scss'],
-    standalone: true,
-    imports: [NgIf, MatProgressSpinnerModule, MatTableModule]
+  selector: 'course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.scss'],
+  standalone: true,
+  imports: [NgIf, MatProgressSpinnerModule, MatTableModule],
 })
 export class CourseComponent implements OnInit {
+  private readonly title = inject(Title);
+  private readonly meta = inject(Meta);
 
+  course: Course;
 
-    course:Course;
+  dataSource: MatTableDataSource<Lesson>;
 
-    dataSource: MatTableDataSource<Lesson>;
+  displayedColumns = ['seqNo', 'description', 'duration'];
 
-    displayedColumns= ["seqNo", "description", "duration"];
+  constructor(
+    private route: ActivatedRoute,
+    private coursesService: CoursesService
+  ) {}
 
+  ngOnInit() {
+    this.course = this.route.snapshot.data['course'];
+    this.title.setTitle(this.course.description);
+    this.meta.addTags([
+      {
+        name: 'description',
+        content: this.course.longDescription,
+      },
+      {
+        property: 'twitter:card',
+        content: 'summary',
+      },
+      {
+        property: 'twitter:site',
+        content: '@AngularUniversal',
+      },
+      {
+        property: 'twitter:title',
+        content: this.course.description,
+      },
+      {
+        property: 'twitter:description',
+        content: this.course.description,
+      },
+      {
+        property: 'twitter:text:description',
+        content: this.course.description,
+      },
+      {
+        property: 'twitter:image',
+        content: 'https://avatars3.githubusercontent.com/u/16628445?v=3&s=200',
+      },
+    ]);
+    this.dataSource = new MatTableDataSource([]);
 
-    constructor(
-        private route: ActivatedRoute,
-        private coursesService: CoursesService) {
-
-    }
-
-
-
-    ngOnInit() {
-
-        this.course = this.route.snapshot.data["course"];
-
-        this.dataSource = new MatTableDataSource([]);
-
-        this.coursesService.findAllCourseLessons(this.course.id)
-            .subscribe(lessons => this.dataSource.data = lessons);
-
-    }
-
-
+    this.coursesService
+      .findAllCourseLessons(this.course.id)
+      .subscribe((lessons) => (this.dataSource.data = lessons));
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,32 +1,31 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {Observable} from "rxjs";
-import {CoursesService} from "../services/courses.service";
-import {map, tap} from 'rxjs/operators';
+import { Component, OnInit, inject } from '@angular/core';
+import { Course } from '../model/course';
+import { Observable } from 'rxjs';
+import { CoursesService } from '../services/courses.service';
+import { map, tap } from 'rxjs/operators';
 import { AsyncPipe } from '@angular/common';
 import { CoursesCardListComponent } from '../courses-card-list/courses-card-list.component';
 import { MatTabsModule } from '@angular/material/tabs';
+import { Title } from '@angular/platform-browser';
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
-    standalone: true,
-    imports: [MatTabsModule, CoursesCardListComponent, AsyncPipe]
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  standalone: true,
+  imports: [MatTabsModule, CoursesCardListComponent, AsyncPipe],
 })
 export class HomeComponent implements OnInit {
+  private readonly title = inject(Title);
 
-    courses$: Observable<Course[]>;
+  courses$: Observable<Course[]>;
 
-    constructor(private coursesService: CoursesService) {
+  constructor(private coursesService: CoursesService) {}
 
-    }
-
-    ngOnInit() {
-        this.courses$ = this.coursesService.findAllCourses()
-            .pipe(
-                map(Object.values)
-            );
-    }
-
+  ngOnInit() {
+    this.title.setTitle('Angular Universal - All Courses');
+    this.courses$ = this.coursesService
+      .findAllCourses()
+      .pipe(map(Object.values));
+  }
 }
